010b8dbfd0bca7cdcfc0fbd5f8fa7d11e91795e2
ArtPlatosha <zipex98@gmail.com>
2023-05-08 05:45:52 +0300

    commit: 9th commit
    activating the hamb. menu (JS)

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// acitvate the wevsite hamburger and navigation
+const hamburger = document.querySelector('.hamb');
+
+hamburger.addEventListener('click', function() {
+	hamburger.classList.toggle('active');
+});
