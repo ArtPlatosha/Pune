a299703e9a61da81652473258caf68338613ee3d
ArtPlatosha <zipex98@gmail.com>
2023-05-08 06:04:38 +0300

    commit: 11th commit
    activating the mobile navbar (JS)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // acitvate the wevsite hamburger and navigation
 const hamburger = document.querySelector('.hamb');
+const nav = document.querySelector('.nav-mobile');
 
 hamburger.addEventListener('click', function() {
 	hamburger.classList.toggle('active');
+	nav.classList.toggle('slide');
 });
